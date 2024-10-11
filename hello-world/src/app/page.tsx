89f9f2ca67e4791e09fc52d6013c45753f2270a3
Nikhil-Jalandhra <jneek36@gmail.com>
2024-10-11 22:21:12 +0530

    minor change

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="w-[60%] text-center">
         <p>I am thrilled about the possibilities that Next.js is set to unlock for developers everywhere! This powerful framework not only streamlines the development process but also enhances performance and scalability, allowing us to create dynamic web applications with ease. With features like server-side rendering, static site generation, and API routes, Next.js empowers us to build faster, more efficient applications that can handle modern web demands. I can&apos;t wait to dive in and explore all the innovative capabilities it offers, transforming the way we approach web development!</p>
         </div>
-        <Image className="w-[300px] mt-[20px]" src={babyGif} alt="excited"/>
+        <Image className="w-[300px] mt-[30px]" src={babyGif} alt="excited"/>
       </div>
       
   );
