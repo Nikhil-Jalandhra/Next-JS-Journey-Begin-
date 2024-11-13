6a0c58d664e5a6d982249eb179b43658560a4357
Nikhil-Jalandhra <jneek36@gmail.com>
2024-11-13 22:04:59 +0530

    almost covered

--- next15/src/app/page.js
+++ next15/src/app/page.js
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
    <div>
-    <h1>Exicted to learn new features of Next-15</h1>
+    <h1>Exicted to learn new features of Next-15
+      I think almost done to move on new things.
+    </h1>
    </div>
   );
 }
